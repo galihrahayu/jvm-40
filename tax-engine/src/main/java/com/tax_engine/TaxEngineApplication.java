ff510e1eef1134798c3d0efe5e938525531e273b
Galih Rahayu <galih.rahayu@online-pajak.com>
2020-12-01 19:29:34 +0700

    Add tax engine retry

--- tax-engine/src/main/java/com/tax_engine/TaxEngineApplication.java
+++ tax-engine/src/main/java/com/tax_engine/TaxEngineApplication.java
@@ -2,7 +2,8 @@ package com.tax_engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.retry.annotation.EnableRetry;
+@EnableRetry
 @SpringBootApplication
 public class TaxEngineApplication {
 
--- tax-engine/src/main/java/com/tax_engine/TaxEngineService.java
+++ tax-engine/src/main/java/com/tax_engine/TaxEngineService.java
@@ -1,13 +1,18 @@
 package com.tax_engine;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+import org.springframework.ws.soap.client.SoapFaultClientException;
+import org.springframework.retry.annotation.Backoff;
 
 @Service
 public class TaxEngineService {
 	@Autowired
 	private NpwpActivatorClient npwpActivatorClient;
 
+	@Retryable(value = {
+			SoapFaultClientException.class }, maxAttempts = 2, backoff = @Backoff(delay = 100, maxDelay = 500))
 	public boolean activate(String npwpToActivate) {
 		Npwp npwp = new Npwp(npwpToActivate);
 		return npwpActivatorClient.activate(npwp.format()).isSuccess();
