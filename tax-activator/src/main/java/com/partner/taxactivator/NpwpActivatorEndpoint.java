8d1f3707c59b81e5735c5a1c87adbec4c09c60bc
Galih Rahayu <galih.rahayu@online-pajak.com>
2020-12-01 19:28:39 +0700

    Simulate internal random error

--- tax-activator/src/main/java/com/partner/taxactivator/NpwpActivatorEndpoint.java
+++ tax-activator/src/main/java/com/partner/taxactivator/NpwpActivatorEndpoint.java
@@ -1,5 +1,7 @@
 package com.partner.taxactivator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -10,12 +12,24 @@ import com.partner.npwp.activator.ActivateNpwpResponse;
 
 @Endpoint
 public class NpwpActivatorEndpoint {
+	private static final Logger log = LoggerFactory.getLogger(NpwpActivatorEndpoint.class);
 	private static final String NAMESPACE_URI = "http://partner.com/npwp/activator";
+
+	// we utilize spring singleton
+	private boolean internalRandomError;
 
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "activateNpwpRequest")
 	@ResponsePayload
-	public ActivateNpwpResponse activateNpwp(@RequestPayload ActivateNpwpRequest request) {
+	public ActivateNpwpResponse activateNpwp(@RequestPayload ActivateNpwpRequest request) throws Exception {
 		ActivateNpwpResponse response = new ActivateNpwpResponse();
+
+		// next random error
+		internalRandomError = !internalRandomError;
+
+		if (internalRandomError) {
+			log.info("Internal Random error triggered!");
+			throw new Exception("Internal Random error!");
+		}
 
 		// validation to check valid NPWP
 		if (request.getNpwp().contains(".") && request.getNpwp().contains("-")) {
